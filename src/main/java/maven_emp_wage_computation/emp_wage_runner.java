1cacd437b89dc129824620f5026f5c76313933e5
Shubham <sm.phoudjar@gmail.com>
2021-03-10 22:31:31 +0530

    Added : Welcome message added.

---
+++ src/main/java/maven_emp_wage_computation/emp_wage_runner.java
@@ -0,0 +1,7 @@
+package maven_emp_wage_computation;
+
+public class emp_wage_runner {
+		public static void main(String[] args) {
+			System.out.println("Hi, Welcome to Employee Wage Computation Program!");
+		}
+}
