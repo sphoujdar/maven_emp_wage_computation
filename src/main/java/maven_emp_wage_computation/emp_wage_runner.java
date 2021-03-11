0b27c85a8a91137ea33f7dabd6be7c83e6df8f12
Shubham <sm.phoudjar@gmail.com>
2021-03-11 08:27:16 +0530

    Added : Switch Case instead of multiple ifs.

--- src/main/java/maven_emp_wage_computation/emp_wage_runner.java
+++ src/main/java/maven_emp_wage_computation/emp_wage_runner.java
@@ -1,24 +1,31 @@
 package maven_emp_wage_computation;
 
 public class emp_wage_runner {
-		public static void main(String[] args) {
-			//Constants
-			int IS_PART_TIME = 1;
-			int IS_FULL_TIME = 2;
-			int EMP_RATE_PER_HOUR = 20;
+		
+	//Constants
+	public static final int IS_PART_TIME = 1;
+	public static final int IS_FULL_TIME = 2;
+	public static final int EMP_RATE_PER_HOUR = 20;
+	
+	
+	public static void main(String[] args) {
 			
 			//Variables
 			int empHrs = 0;
 			int empWage = 0;
 			
 			//Computation
-			double empcheck = Math.floor(Math.random() * 10) % 3;
-			if (empcheck == IS_FULL_TIME)
-				empHrs = 8;
-			else if (empcheck == IS_PART_TIME)
-				empHrs = 4;
+			int empcheck = (int) Math.floor(Math.random() * 10) % 3;
 			
+			switch (empcheck) { 
+			case IS_FULL_TIME:
+				empHrs = 8;
+				break;
+			case IS_PART_TIME:
+				empHrs = 4;
+				break;
+			}
 			empWage = empHrs*EMP_RATE_PER_HOUR;
-			System.out.println("Emp Wage :" + empWage);
+			System.out.println("Emp Wage : " + empWage);
 		}
 }
