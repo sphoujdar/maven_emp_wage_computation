d3175dfeaf50992da03ac8c393d2816953efa1a0
Shubham <sm.phoudjar@gmail.com>
2021-03-11 08:04:52 +0530

    Added : Part Time wage Logic.

--- src/main/java/maven_emp_wage_computation/emp_wage_runner.java
+++ src/main/java/maven_emp_wage_computation/emp_wage_runner.java
@@ -3,7 +3,8 @@ package maven_emp_wage_computation;
 public class emp_wage_runner {
 		public static void main(String[] args) {
 			//Constants
-			int IS_FULL_TIME = 1;
+			int IS_PART_TIME = 1;
+			int IS_FULL_TIME = 2;
 			int EMP_RATE_PER_HOUR = 20;
 			
 			//Variables
@@ -11,11 +12,11 @@ public class emp_wage_runner {
 			int empWage = 0;
 			
 			//Computation
-			double empcheck = Math.floor(Math.random() * 10) % 2;
+			double empcheck = Math.floor(Math.random() * 10) % 3;
 			if (empcheck == IS_FULL_TIME)
 				empHrs = 8;
-			else
-				empHrs = 0;
+			else if (empcheck == IS_PART_TIME)
+				empHrs = 4;
 			
 			empWage = empHrs*EMP_RATE_PER_HOUR;
 			System.out.println("Emp Wage :" + empWage);
