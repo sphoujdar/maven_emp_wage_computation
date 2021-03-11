1a7fb2571467a00f0e37c734d3425728f31c5aee
Shubham <sm.phoudjar@gmail.com>
2021-03-11 07:36:28 +0530

    Added : Attendance Check Logic.

--- src/main/java/maven_emp_wage_computation/emp_wage_runner.java
+++ src/main/java/maven_emp_wage_computation/emp_wage_runner.java
@@ -2,6 +2,14 @@ package maven_emp_wage_computation;
 
 public class emp_wage_runner {
 		public static void main(String[] args) {
-			System.out.println("Hi, Welcome to Employee Wage Computation Program!");
+			//Constants
+			int IS_FULL_TIME = 1;
+			
+			//Computation
+			double empcheck = Math.floor(Math.random() * 10) % 2;
+			if (empcheck == IS_FULL_TIME)
+				System.out.println("Employee is Present.");
+			else
+				System.out.println("Employee is Absent.");
 		}
 }
