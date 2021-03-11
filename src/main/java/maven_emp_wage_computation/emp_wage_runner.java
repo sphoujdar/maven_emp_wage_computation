c303705be493a69b94ad336f956f6b759bc676e6
Shubham <sm.phoudjar@gmail.com>
2021-03-11 13:12:38 +0530

    Refactored : Correction, added default to switch statement so that program worked correctly.

--- src/main/java/maven_emp_wage_computation/emp_wage_runner.java
+++ src/main/java/maven_emp_wage_computation/emp_wage_runner.java
@@ -7,16 +7,16 @@ public class emp_wage_runner {
 		public static final int IS_FULL_TIME = 2;
 		public static final int EMP_RATE_PER_HOUR = 20;
 		public static final int NUM_WORKING_DAYS = 20;
+		public static final int MAX_WORKING_HRS = 100;
 		
 		
 		public static void main(String[] args) {
 				
 				//Variables
-				int empHrs = 0, empWage = 0 , totalEmpWage = 0, totalEmpHrs = 0;
-				int day = 0 ;
+				int empHrs = 0, empWage = 0 , totalEmpWage = 0, totalEmpHrs = 0, day_of_month = 0 ;
 	
 				//Computation
-				while ( day < NUM_WORKING_DAYS && totalEmpHrs < 101 ){
+				while ( day_of_month < NUM_WORKING_DAYS && totalEmpHrs <= MAX_WORKING_HRS ){
 					
 						int empcheck = (int) Math.floor(Math.random() * 10) % 3;
 						
@@ -27,13 +27,15 @@ public class emp_wage_runner {
 						case IS_PART_TIME:
 							empHrs = 4;
 							break;
+						default:
+							empHrs = 0;
 						}
 						empWage = empHrs*EMP_RATE_PER_HOUR;
 						totalEmpWage += empWage; 
 						totalEmpHrs += empHrs;
-						day++;
+						day_of_month++;
 						
-				System.out.printf("Employee's Wage for day %d is : %d\n",day, empWage);
+				System.out.printf("Employee's Wage for day %d is : %d\n",day_of_month, empWage);
 				}
 				
 				System.out.printf("\nEmployee's Wage for the month is %d\n" , totalEmpWage);
