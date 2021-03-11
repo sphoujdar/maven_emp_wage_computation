40a059f4fa81b264a1113cdc99bec48e14b88dfd
Shubham <sm.phoudjar@gmail.com>
2021-03-11 12:56:15 +0530

    Refactored : Extended program to calculate wages for a max. 100 hours or 20 days , whichever comes first.

--- src/main/java/maven_emp_wage_computation/emp_wage_runner.java
+++ src/main/java/maven_emp_wage_computation/emp_wage_runner.java
@@ -6,19 +6,17 @@ public class emp_wage_runner {
 		public static final int IS_PART_TIME = 1;
 		public static final int IS_FULL_TIME = 2;
 		public static final int EMP_RATE_PER_HOUR = 20;
-		public static final int NUM_WORKING_DAYS = 4;
+		public static final int NUM_WORKING_DAYS = 20;
 		
 		
 		public static void main(String[] args) {
 				
 				//Variables
-				int empHrs = 0;
-				int empWage = 0;
-				int totalEmpWage = 0;
-				
+				int empHrs = 0, empWage = 0 , totalEmpWage = 0, totalEmpHrs = 0;
+				int day = 0 ;
 	
 				//Computation
-				for (int day = 0 ; day < NUM_WORKING_DAYS ; day++){
+				while ( day < NUM_WORKING_DAYS && totalEmpHrs < 101 ){
 					
 						int empcheck = (int) Math.floor(Math.random() * 10) % 3;
 						
@@ -31,10 +29,14 @@ public class emp_wage_runner {
 							break;
 						}
 						empWage = empHrs*EMP_RATE_PER_HOUR;
-						totalEmpWage += empWage;
-				System.out.printf("Emp Wage for day %d is : %d\n",day, empWage);
+						totalEmpWage += empWage; 
+						totalEmpHrs += empHrs;
+						day++;
+						
+				System.out.printf("Employee's Wage for day %d is : %d\n",day, empWage);
 				}
 				
-				System.out.printf("\nEmp Wage for the month is %d\n" , totalEmpWage);
+				System.out.printf("\nEmployee's Wage for the month is %d\n" , totalEmpWage);
+				System.out.printf("Employee Worked for %d hours\n" , totalEmpHrs);
 		}
 }
