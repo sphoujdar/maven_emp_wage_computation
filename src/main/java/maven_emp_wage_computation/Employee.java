3ccb1fd3257e28546f4195a9bec43ad0abab1b3f
Shubham <sm.phoudjar@gmail.com>
2021-03-12 06:04:45 +0530

    Refarctored : Changed code to use class variables and class methods.

--- src/main/java/maven_emp_wage_computation/Employee.java
+++ src/main/java/maven_emp_wage_computation/Employee.java
@@ -3,9 +3,22 @@ package maven_emp_wage_computation;
 public class Employee {
 	
 	//Class Variables
-	int empHrs = 0, empWage = 0 , totalEmpWage = 0, totalEmpHrs = 0, total_days_worked = 0 ;
+	int totalEmpWage, totalEmpHrs, total_days_worked;
+
 	
-	public static int calc_employee_wage(Employee employee1) {
+	//Constructor to initialize values
+	public Employee(int totalEmpWage, int totalEmpHrs, int total_days_worked) {
+		super();
+		this.totalEmpWage = totalEmpWage;
+		this.totalEmpHrs = totalEmpHrs;
+		this.total_days_worked = total_days_worked;
+	}
+
+
+	//Class Method to calculate employee wage
+	public void calc_employee_wage(Employee employee1) {
+		
+		int empHrs = 0, empWage = 0; 
 		
 		while ( employee1.total_days_worked < emp_wage_runner.NUM_WORKING_DAYS 
 				&& employee1.totalEmpHrs <= emp_wage_runner.MAX_WORKING_HRS ){
@@ -23,14 +36,16 @@ public class Employee {
 				default:
 					empHrs = 0;
 				}
-				empWage = empHrs*EMP_RATE_PER_HOUR;
-				totalEmpWage += empWage; 
-				totalEmpHrs += empHrs;
-				total_days_worked++;
+				empWage = empHrs*emp_wage_runner.EMP_RATE_PER_HOUR;
+				employee1.totalEmpWage += empWage; 
+				employee1.totalEmpHrs += empHrs;
+				employee1.total_days_worked++;
 				
-		System.out.printf("Employee's Wage for day %d is : %d\n",total_days_worked, empWage);
+		System.out.printf("Employee's Wage for day %d is : %d\n",employee1.total_days_worked, empWage);
 		}
 		
+		System.out.printf("\nEmployee's Wage for the month is %d\n" , employee1.totalEmpWage);
+		System.out.printf("Employee Worked for %d hours\n" , employee1.totalEmpHrs);
 		
 	}
 }
--- src/main/java/maven_emp_wage_computation/emp_wage_runner.java
+++ src/main/java/maven_emp_wage_computation/emp_wage_runner.java
@@ -12,33 +12,8 @@ public class emp_wage_runner {
 		
 		public static void main(String[] args) {
 	
-				Employee employee1;
-			
-			
-				//Computation
-				while ( employee1.total_days_worked < NUM_WORKING_DAYS && employee1.totalEmpHrs <= MAX_WORKING_HRS ){
-					
-						int empcheck = (int) Math.floor(Math.random() * 10) % 3;
-						
-						switch (empcheck) { 
-						case IS_FULL_TIME:
-							empHrs = 8;
-							break;
-						case IS_PART_TIME:
-							empHrs = 4;
-							break;
-						default:
-							empHrs = 0;
-						}
-						empWage = empHrs*EMP_RATE_PER_HOUR;
-						totalEmpWage += empWage; 
-						totalEmpHrs += empHrs;
-						total_days_worked++;
-						
-				System.out.printf("Employee's Wage for day %d is : %d\n",total_days_worked, empWage);
-				}
+				Employee employee1 = new Employee(0,0,0);
+				employee1.calc_employee_wage(employee1);
 				
-				System.out.printf("\nEmployee's Wage for the month is %d\n" , totalEmpWage);
-				System.out.printf("Employee Worked for %d hours\n" , totalEmpHrs);
 		}
 }
